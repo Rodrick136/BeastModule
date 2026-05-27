527d964c988606f7e32d4227a4092ff047b5bac3
Rodrick Dusio MacDermott <rodrickdusio+github@gmail.com>
2026-05-26 20:08:25 -0700

    Add build_release.ts for automated build process and update module.json with license and download link

---
+++ build_release.ts
@@ -0,0 +1,15 @@
+/// <reference types="bun" />
+export { }
+
+console.log("Building release...");
+
+// run build_module.ts
+await Bun.$`bun run ./build_module.ts`;
+
+const build = "./build" as const;
+
+// take build and zip it to dist
+const dist = "./dist" as const;
+await Bun.$`rm -rf ${dist}/`.quiet();
+await Bun.$`mkdir -p ${dist}/`.quiet();
+await Bun.$`zip -r ${dist}/beast.zip ${build}/*`.quiet();
