6325618e5a1d2501f25102b64420506bc8654859
Rodrick Dusio MacDermott <rodrickdusio+github@gmail.com>
2026-05-26 20:09:43 -0700

    Fix build_release.ts to ensure manifest is copied to dist after zipping

--- build_release.ts
+++ build_release.ts
@@ -13,3 +13,6 @@ const dist = "./dist" as const;
 await Bun.$`rm -rf ${dist}/`.quiet();
 await Bun.$`mkdir -p ${dist}/`.quiet();
 await Bun.$`zip -r ${dist}/beast.zip ${build}/*`.quiet();
+
+// copy manifest to dist
+await Bun.$`cp ./src/module.json ${dist}/`.quiet();
